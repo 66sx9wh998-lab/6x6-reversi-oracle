45cdb1263883464599b6aa25fbcb6680d92cbf5a
66sx9wh998-lab <66sx9wh998@privaterelay.appleid.com>
2026-01-04 04:29:12 +0900

    Update values.ts

--- app/src/ui/values.ts
+++ app/src/ui/values.ts
@@ -1,47 +1,47 @@
-import * as THREE from 'three';
+import * as UI from './ui';
+import * as Query from './query';
+import * as Board from './board';
 
-export type Values = {
-  update: (values: (number | null)[]) => void;
+/* 黒の手 */
+const doBlackMove = (moves: number[], blackMove: number) => {
+  const newMoves = moves.slice().concat([blackMove]);
+  query.request(newMoves);
+  ui.enqueue(newMoves, Board.createBoard(newMoves));
 };
 
-export const init = (scene: THREE.Scene): Values => {
-  const canvas = document.createElement('canvas');
-  canvas.width = canvas.height = 600;
-  const ctx = canvas.getContext('2d');
-  if (!ctx) throw 'failed to getContext of canvas';
+/* 白の手 */
+const doWhiteMove = (moves: number[], whiteMove: number) => {
+  const newMoves = moves.slice().concat([whiteMove]);
+  query.request(newMoves);
+  ui.enqueue(newMoves, Board.createBoard(newMoves));
+};
 
-  ctx.fillStyle = '#2e674c';
-  ctx.font = '16px sans-serif';
-  const geometry = new THREE.PlaneGeometry(6, 6);
-  const texture = new THREE.CanvasTexture(canvas);
-  const material = new THREE.MeshBasicMaterial({
-    map: texture,
-    transparent: true,
+/* UI 初期化 */
+const ui = UI.init(doBlackMove, doWhiteMove);
+
+/* AI評価値を受け取る */
+const blackMovesListener = (
+  moves: number[],
+  blackMoves: [number, number][]
+) => {
+  const valuesArray: (number | null)[] = Array(36).fill(null);
+
+  blackMoves.forEach(([move, value]) => {
+    if (move >= 0) {
+      valuesArray[move] = value;
+    }
   });
-  const mesh = new THREE.Mesh(geometry, material);
 
-  mesh.rotation.x = -Math.PI / 2;
-  mesh.position.y = 0.001;
-
-  scene.add(mesh);
-
-  return {
-    update: (values: (number | null)[]) => {
-      ctx.clearRect(0, 0, 600, 600);
-      for (let y = 0; y < 6; y++) {
-        for (let x = 0; x < 6; x++) {
-          const value = values[x + y * 6];
-          if (value !== null) {
-            const str = '' + value;
-            ctx.fillText(
-              str,
-              100 * x + 100 - ctx.measureText(str).width - 4,
-              100 * y + 100 - 8
-            );
-          }
-        }
-      }
-      if (material.map) material.map.needsUpdate = true;
-    },
-  };
+  ui.updateValues(valuesArray);
+  ui.enqueue(moves, Board.createBlackBoard(moves, blackMoves));
 };
+
+/* 白の手（連鎖なし） */
+const whiteMoveListener = (moves: number[], whiteMove: number) => {
+  const newMoves = moves.slice().concat([whiteMove]);
+  ui.enqueue(newMoves, Board.createBoard(newMoves));
+};
+
+/* Query 初期化 */
+const query = Query.init(blackMovesListener, whiteMoveLis
+
